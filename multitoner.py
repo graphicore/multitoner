d42acf0cd127fd8cbb1e94dd49ce5660b03186dc
Lasse Fister <commander@graphicore.de>
2013-09-03 19:13:44 +0200

    make new documents and open them in a GtkNotebook-Tab

--- multitoner.py
+++ multitoner.py
@@ -4,6 +4,7 @@
 from __future__ import division
 import sys
 from gi.repository import Gtk, Gdk, GObject
+from gtkinktool import InksEditor, ModelCurves, ModelInk, History, GradientWorker
 
 # just a preparation for i18n
 def _(string):
@@ -21,24 +22,65 @@ UI_INFO = """
 </ui>
 """
 
-class Multitoner(Gtk.Grid):
-    def __init__(self):
+class Label(Gtk.Grid):
+    __gsignals__ = {
+        'close': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, (
+                  # id
+                  GObject.TYPE_INT, ))
+    }
+    def __init__(self, id, text):
         Gtk.Grid.__init__(self)
+        self.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.set_column_spacing(5)
+        #self.set_row_spacing(5)
+        
+        
+        # icon
+        icon = Gtk.Image.new_from_stock(Gtk.STOCK_FILE, Gtk.IconSize.MENU)
+        self.attach(icon, 0, 0, 1, 1)
+        
+        # label 
+        label = Gtk.Label(text)
+        self.attach_next_to(label, icon, Gtk.PositionType.RIGHT, 1, 1)
+        
+        # close button
+        button = Gtk.Button()
+        button.set_focus_on_click(False)
+        button.add(Gtk.Image.new_from_stock(Gtk.STOCK_CLOSE, Gtk.IconSize.MENU))
+        button.connect('clicked', self.clickedHandler, id)
+        self.attach_next_to(button, label, Gtk.PositionType.RIGHT, 1, 1)
+        self.show_all()
+    def clickedHandler(self, button, id, data=None):
+        self.emit("close", id)
 
+class Multitoner(Gtk.Grid):
+    def __init__(self, documents):
+        Gtk.Grid.__init__(self)
+        self._gradientWorker = GradientWorker()
+        self._documents = documents
+        self._activeDocument = None
         #self.documentActions = Gtk.ActionGroup('document_actions')
         
-        self.globalActions = self.makeGlobalActions()
-
+        self._globalActions = self.makeGlobalActions()
+        menubar = self.initMenu()
+        self.attach(menubar, 0, 0, 1, 1)
+        self._notebook = Gtk.Notebook()
+        self._notebook.set_scrollable(True)
+        def switchPageHandler(widget, page, page_num):
+            print 'switchPage', page, page_num
+            self._activeDocument = id(widget)
+            return True
+            
+        self._notebook.connect('switch-page', switchPageHandler)
+        self.attach(self._notebook, 0, 1, 1, 1)
+        
+    def initMenu(self):
         uimanager = Gtk.UIManager()
         uimanager.add_ui_from_string(UI_INFO)
+        uimanager.insert_action_group(self._globalActions)
+        menubar = uimanager.get_widget("/MenuBar")
         
-        uimanager.insert_action_group(self.globalActions)
-        
-        menubar = uimanager.get_widget("/MenuBar")        
-        self.attach(menubar, 0, 0, 1, 1)
-
         #toolbar = uimanager.get_widget("/ToolBar")
-        
         # this removes the immediate dependency to window
         def onRealize(widget, *args):
             """ 
@@ -52,22 +94,46 @@ class Multitoner(Gtk.Grid):
             widget.disconnect(realize_handler_id)
         #save realize_handler_id for the closure of onRealize 
         realize_handler_id = self.connect('realize' , onRealize)
+        return menubar
 
     def makeGlobalActions(self):
         actionGroup = Gtk.ActionGroup('global_actions')
         actionGroup.add_actions([
               ('FileMenu', None, 'File', None,
                None, None)
-            , ('FileNew', Gtk.STOCK_NEW, 'New …', '<Ctrl>n',
-               'Start a new document.', self.menuFileNewHandler)
-            , ('FileQuit', Gtk.STOCK_QUIT, 'Quit', '<ctrl>q',
+            , ('FileNew', Gtk.STOCK_NEW, _('New …'), '<Ctrl>n',
+               _('Start a new document.'), self.menuFileNewHandler)
+            , ('FileQuit', Gtk.STOCK_QUIT, _('Quit'), '<ctrl>q',
                None, self.menuFileQuitHandler)
             ])
         return actionGroup
     
     def menuFileNewHandler(self, widget):
-        print 'new file handler'
+        self.makeNewDocument()
     
+    def closeDocumentHandler(self, widget, id, *data):
+        page = self._notebook.page_num(self._documents[id]['widget'])
+        self._notebook.remove_page(page)
+        # Todo: ask when there are unsafed changes
+        del self._documents[id]
+    
+    def makeNewDocument(self):
+        model = ModelCurves(ChildModel=ModelInk)
+        history = History(model)
+        inksEditor = InksEditor(model, self._gradientWorker)
+        widget_id = id(inksEditor)
+        self._documents[widget_id] = {
+            'history': history,
+            'model': model,
+            'widget': inksEditor
+        }
+        label = Label(widget_id, _('new Document*'))
+        label.connect('close', self.closeDocumentHandler, widget_id)
+        self._notebook.append_page(inksEditor, label)
+        self._notebook.set_tab_reorderable(inksEditor, True)
+        inksEditor.show_all()
+        return widget_id
+        
     def menuFileQuitHandler(self, widget):
         Gtk.main_quit()
 
@@ -89,7 +155,8 @@ if __name__ == '__main__':
     styleContext.add_provider_for_screen(screen, cssProvider,
         Gtk.STYLE_PROVIDER_PRIORITY_USER)
     
-    multitoner = Multitoner()
+    documents = {}
+    multitoner = Multitoner(documents)
     window.add(multitoner)
     
     window.show_all()
