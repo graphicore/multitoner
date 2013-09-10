3a6cb0774151efc333b6aba6d728dcff33aa16fb
Lasse Fister <commander@graphicore.de>
2013-09-10 03:27:20 +0200

    start to add the menu, scaling functionalities

--- PreviewWindow.py
+++ PreviewWindow.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 from __future__ import division, print_function, unicode_literals
 
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, GObject, Gdk
 import cairo
 from PreviewWorker import PreviewWorker
+from emitter import Emitter
 from weakref import ref as Weakref
 import math
 
@@ -14,34 +14,181 @@ import math
 def _(string):
     return string
 
+UI_INFO = """
+<ui>
+  <menubar name='MenuBar'>
+    <menu action="EditMenu">
+      <menuitem action='ZoomIn' />
+      <menuitem action='ZoomOut' />
+      <menuitem action='ZoomUnit' />
+    </menu>
+  </menubar>
+  <toolbar name="ToolBar">
+      <toolitem action='ZoomIn' />
+      <toolitem action='ZoomOut' />
+      <toolitem action='ZoomUnit' />
+  </toolbar>
+</ui>
+"""
+
 class Canvas(Gtk.Viewport):
     def __init__(self, *args):
         Gtk.Viewport.__init__(self, *args)
         
-        self.scale = 1
-        
         self.surface = None
-        self.widh = 0
+        self.width = 0
         self.height = 0
+        
+        self.scaleToFit = False
+        
+        self._center = None
+        self._restoringCenter = False
+        self._timers = {}
         
         self.da = Gtk.DrawingArea()
         self.add(self.da)
+        self._initEventHandlers()
+        
+        self.drawCounter = 0
+    
+    def _initEventHandlers(self):
         self.da.connect('draw' , self.onDraw)
+        self.da.add_events(
+            Gdk.EventMask.STRUCTURE_MASK # needed for configure-event
+        )
+        self.da.connect('configure-event', self.configureHandler)
+        self.get_vadjustment().connect(
+            'value-changed', self.adjustmentValueChangedHandler)
+        self.get_hadjustment().connect(
+            'value-changed', self.adjustmentValueChangedHandler)
+        
+        # this will keep the center of the image in the center of the window
+        # when the window is beeing resized
+        self.add_events(
+            Gdk.EventMask.STRUCTURE_MASK # needed for configure-event
+        )
+        def onRealize(widget, *args):
+            """ 
+            closure to connect to window when it establishes this widget
+            """
+            window = widget.get_toplevel()
+            window.connect('configure-event', self.toplevelConfigureHandler)
+            
+            parent = widget.get_parent()
+            parent.connect('size-allocate', self.parentSizeAllocateHandler)
+            
+            widget.disconnect(realize_handler_id)
+        # save realize_handler_id for the closure of onRealize 
+        realize_handler_id = self.connect('realize' , onRealize)
     
     def receiveSurface(self, surface, width, height):
         self.surface = surface
         self.width = width
         self.height = height
-        self._resize()
+        
+        if not hasattr(self, '_scale'):
+            parent_allocation = self.get_parent().get_allocation()
+            self._scaleToFit(parent_allocation.width, parent_allocation.height)
+        else:
+            self._resize()
         self.da.queue_draw()
     
     def _resize(self):
+        print ('resize')
         self.da.set_size_request(
             math.ceil(self.width * self.scale),
             math.ceil(self.height * self.scale)
         )
     
+    def configureHandler(self, widget, event):
+        """
+        the configure event signals when the DrawingArea got resized
+        happens after receiveSurface and can be handled immediately
+        """
+        if not self.scaleToFit:
+            self._restoreCenter()
+    
+    """ when the toplevel window got resized """
+    toplevelConfigureHandler = configureHandler
+    
+    def parentSizeAllocateHandler(self, parent, allocation):
+        if self.scaleToFit:
+            self._scheduleScaleToFit(allocation.width, allocation.height)
+    
+    def adjustmentValueChangedHandler(self, adjustment):
+        self._saveCenter()
+    
+    def _saveCenter(self):
+        if self._restoringCenter == True:
+            return
+        center = (
+        #   (scrollbar position                 + screen width                 / 2)) / image width
+            (self.get_hadjustment().get_value() + (self.get_allocated_width()  / 2)) / self.da.get_allocated_width()
+        #   (scrollbar position                 + screen height                / 2)) / image height
+          , (self.get_vadjustment().get_value() + (self.get_allocated_height() / 2)) / self.da.get_allocated_height()
+        )
+        self._center = center
+        return center
+    
+    def _restoreCenter(self):
+        if self._center is None:
+            return
+        self._restoringCenter = True
+        try:
+            h, v = self._center
+            #      image width                    * center of view - screen width   / 2
+            left = self.da.get_allocated_width()  * h - self.get_allocated_width()  / 2
+            #      image height                   * center of view - screen height  / 2
+            top  = self.da.get_allocated_height() * v - self.get_allocated_height() / 2
+            self.get_hadjustment().set_value(left)
+            self.get_vadjustment().set_value(top)
+        finally:
+            self._restoringCenter = False
+    
+    @property
+    def scale(self):
+        return getattr(self, '_scale', 1)
+    
+    @scale.setter
+    def scale(self, value):
+        if self.scale == value:
+            return
+        self._scale = value
+        self._saveCenter()
+        self._resize()
+        self.da.queue_draw()
+    
+    def _schedule(self, callback, time, *args, **kwds):
+        timer = self._timers.get(callback, None)
+        if timer is not None:
+            # remove the old timer
+            GObject.source_remove(timer)
+        # schedule a new execution
+        timer = GObject.timeout_add(time, callback, *args, **kwds)
+        # remember the new timer
+        self._timers[callback] = timer
+    
+    def _scheduleScaleToFit(self, width, height):
+        self._schedule(self._scaleToFit, 300, width, height)
+    
+    def _scaleToFit(self, available_width, available_height):
+        try:
+            aspect_ratio = self.width / self.height
+            available_aspect_ratio = available_width / available_height
+        except ZeroDivisionError:
+            self.scale = 1
+        else:
+            if aspect_ratio > available_aspect_ratio:
+                # fit to width
+                self.scale = available_width/self.width
+            else: 
+                # fit to height
+                self.scale = available_height/self.height
+    
     def onDraw(self, da, cr):
+        print ('onDraw', self.drawCounter)
+        self.drawCounter+=1
+        
         width = self.get_allocated_width()
         height =  self.get_allocated_height()
         left = math.floor(self.get_hadjustment().get_value())
@@ -57,6 +204,32 @@ class Canvas(Gtk.Viewport):
             # draws just the visible area
             cr.rectangle(left, top, width, height)
             cr.fill()
+
+class CanvasControls(Emitter):
+    def __init__(self, canvas):
+        self.canvas = canvas
+    
+    def zoomIn(self):
+        old = self.canvas.scale
+        old = round(old, 2)
+        new = old + 0.05
+        new = min(16, new)
+        
+        self.canvas.scale = new
+    
+    def zoomOut(self):
+        old = self.canvas.scale
+        old = round(old, 2)
+        new = old - 0.05
+        new = max(0.05, new)
+        self.canvas.scale = new
+    
+    def zoomUnit(self):
+        old = self.canvas.scale
+        self.canvas.scale = 1
+    
+    def zoomFit(self):
+        pass
 
 class PreviewWindow(Gtk.Window):
     def __init__(self, inksModel, imageName):
@@ -74,6 +247,9 @@ class PreviewWindow(Gtk.Window):
         self._update_needed = None
         self._noInks = False
         
+        self.grid = Gtk.Grid()
+        self.add(self.grid)
+        
         self.scrolled = Gtk.ScrolledWindow()
         adjustments = (self.scrolled.get_hadjustment(),
                        self.scrolled.get_vadjustment())
@@ -81,12 +257,61 @@ class PreviewWindow(Gtk.Window):
         self.canvas = Canvas(*adjustments)
         self.canvas.set_halign(Gtk.Align.CENTER)
         self.canvas.set_valign(Gtk.Align.CENTER)
+        self.canvasCtrl = CanvasControls(self.canvas)
+        self.canvasCtrl.add(self) # subscribe
         
         self.scrolled.add(self.canvas)
+        self.scrolled.set_hexpand(True)
+        self.scrolled.set_vexpand(True)
         
-        self.add(self.scrolled)
+        self.menubar, self.toolbar = self._initMenu()
+        
+        scaler = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, 0.05, 16.0, 0.05)
+        scaler.set_digits(2)
+        scaler.set_draw_value(True)
+        scaler.set_value(1)
+        def setScale(*args):
+            self.canvas.scale = scaler.get_value()
+        scaler.connect('value-changed', setScale)
+        
+        self.grid.attach(self.menubar, 0, 0, 1, 1)
+        self.grid.attach(self.toolbar, 0, 1, 1, 1)
+        self.grid.attach(scaler,  0, 2, 1, 1)
+        scaler.set_halign(Gtk.Align.FILL)
+        
+        self.grid.attach(self.scrolled, 0, 3, 1, 1)
+        
         self._previewWorker = PreviewWorker()
         self._requestNewSurface(inksModel)
+    
+    def _makeDocumentActions(self):
+        actionGroup = Gtk.ActionGroup('document_actions')
+        actionGroup.add_actions([
+              ('EditMenu', None, _('Edit'), None,
+               None, None)
+            , ('ZoomIn', Gtk.STOCK_ZOOM_IN, _('Zoom In'),  '<Ctrl>plus',
+               None, self.actionZoomInHandler)
+            , ('ZoomOut', Gtk.STOCK_ZOOM_OUT, _('Zoom Out'), '<Ctrl>minus',
+               None, self.actionZoomOutHandler)
+            , ('ZoomUnit', Gtk.STOCK_ZOOM_100, _('Zoom 100%'), '<Ctrl>minus',
+               None, self.actionZoomUnitHandler)
+               # missing: Gtk.STOCK_ZOOM_100, Gtk.STOCK_ZOOM_FIT
+            ])
+        return actionGroup
+    
+    def _initMenu(self):
+        self._documentActions = self._makeDocumentActions()
+        self.UIManager = uimanager = Gtk.UIManager()
+        uimanager.add_ui_from_string(UI_INFO)
+        uimanager.insert_action_group(self._documentActions)
+        menubar = uimanager.get_widget("/MenuBar")
+        toolbar = uimanager.get_widget("/ToolBar")
+        
+        # Add the accelerator group to the toplevel window
+        accelgroup = uimanager.get_accel_group()
+        self.add_accel_group(accelgroup)
+        
+        return menubar, toolbar
     
     def onModelUpdated(self, inksModel, event, *args):
         if len(inksModel.visibleCurves) == 0:
@@ -156,5 +381,14 @@ class PreviewWindow(Gtk.Window):
             if inksModel is not None:
                 self._requestNewSurface(inksModel)
         
-        scale = self.canvas.scale = 1
         self.canvas.receiveSurface(cairo_surface, w, h)
+    
+    # actions
+    def actionZoomInHandler(self, widget):
+        self.canvasCtrl.zoomIn()
+    
+    def actionZoomOutHandler(self, widget):
+        self.canvasCtrl.zoomOut()
+    
+    def actionZoomUnitHandler(self, widget):
+        self.canvasCtrl.zoomUnit()
