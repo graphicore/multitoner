a7ab8425e9d4343d8874ac7d993871ce45b019d3
Lasse Fister <commander@graphicore.de>
2013-09-23 15:41:12 +0200

    add 'save as' dialog, change 'opne image' dialog, add showMessage as factory for error and notice

--- dialogs.py
+++ dialogs.py
@@ -10,33 +10,46 @@ def _(string):
     return string
 
 
-class OpenImageDialog(object):
-    def __init__(self, window):
-        self.window = window
-    
-    def _runFileOpenDialog(self):
-        dialog = Gtk.FileChooserDialog(title=_('Open Image File for Preview')
-            , parent=self.window
-            , action=Gtk.FileChooserAction.OPEN
-            , buttons=( Gtk.STOCK_CANCEL
-                      , Gtk.ResponseType.CANCEL
-                      , Gtk.STOCK_OPEN
-                      , Gtk.ResponseType.ACCEPT
-            )
+def showOpenImageDialog(window):
+    dialog = Gtk.FileChooserDialog(title=_('Choose an Image-File')
+        , parent=window
+        , action=Gtk.FileChooserAction.OPEN
+        , buttons=( Gtk.STOCK_CANCEL
+                    , Gtk.ResponseType.CANCEL
+                    , Gtk.STOCK_OPEN
+                    , Gtk.ResponseType.ACCEPT
         )
-        image_filter = Gtk.FileFilter()
-        image_filter.add_mime_type('image/*')
-        dialog.set_filter(image_filter)
-        
-        filename = None
-        if dialog.run() == Gtk.ResponseType.ACCEPT:
-            filename = dialog.get_filename()
-        dialog.destroy()
-        return filename
+    )
+    image_filter = Gtk.FileFilter()
+    image_filter.add_mime_type('image/*')
+    dialog.set_filter(image_filter)
     
-    def execute(self):
-        filename = self._runFileOpenDialog()
-        return filename
+    filename = None
+    if dialog.run() == Gtk.ResponseType.ACCEPT:
+        filename = dialog.get_filename()
+    dialog.destroy()
+    return filename
+
+def showSaveAsDialog(window, filename=None, current_name=None):
+    dialog = Gtk.FileChooserDialog(title=_('Save File')
+        , parent=window
+        , action=Gtk.FileChooserAction.SAVE
+        , buttons=( Gtk.STOCK_CANCEL
+                    , Gtk.ResponseType.CANCEL
+                    , Gtk.STOCK_SAVE
+                    , Gtk.ResponseType.ACCEPT
+        )
+    )
+    dialog.set_do_overwrite_confirmation(True);
+    if current_name is not None:
+        dialog.set_current_name(current_name)
+    if filename is not None:
+        dialog.set_filename(filename)
+    result = None
+    if dialog.run() == Gtk.ResponseType.ACCEPT:
+        result = dialog.get_filename()
+    dialog.destroy()
+    return result
 
 def _showMessageDialog(message_type, window, message, moreInfo=None):
     dialog = Gtk.MessageDialog(
@@ -62,3 +75,12 @@ def showErrorDialog(window, message, moreInfo=None):
     
 def showNoticeDialog(window, message, moreInfo=None):
     return _showMessageDialog(Gtk.MessageType.INFO, window, message, moreInfo)
+
+def showMessage(window, type, message, moreInfo):
+    showDialog = {
+          'error': showErrorDialog
+        , 'notice': showNoticeDialog
+    }.get(type, None)
+    assert type is not None, 'There is no dialog for message type {0}'.format(type)
+    
+    return showDialog(window, message, moreInfo)
