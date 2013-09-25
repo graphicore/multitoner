037b13ae005227cbb0d84d5f9d94b8bb3265901f
Lasse Fister <commander@graphicore.de>
2013-09-25 17:16:45 +0200

    comments and cleanup

--- emitter.py
+++ emitter.py
@@ -2,20 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
+
 from weakref import WeakSet
 
 __all__ = ['Emitter']
 
+
 class Emitter(object):
-    """
-        simple event subscription
-        important:
-            1. this is uses a set, so there is no guaranteed order
-            2. the subscriber needs to implement all callbacks of the actual Emitter
-        
-        to subscribe use emitterObj.add
-        to unsubscribe use emitterObj.remove or emitterObj.discard or
-           delete all references to the subscriber
+    """simple event subscription
+    
+    The subscribers are stored in a weakref.WeakSet, so:
+        - Deleting all references to a subscriberend will end the subscription.
+        - There will never be more than one subscription per object, no
+          matter how often it was subscribed via add()
+        - There's no guaranteed order of execution. E.g. the last subscriber
+          may get called first.
+    
+    IMPORTANT:  the subscriber needs to implement all callbacks of the
+    concrete Emitter.
+    
+    to subscribe use emitterObj.add
+    to unsubscribe use emitterObj.remove or emitterObj.discard or
+        delete all references to the subscriber
     """
     @property
     def _subscriptions(self):
@@ -24,16 +32,33 @@ class Emitter(object):
         return self.__subscriptions
     
     def add(self, thing):
+        """ Subscribe to this emmiter.
+        
+        Be warned that the Emitter expects certain callback methods to exist
+        in the subscribing object. The name of the methods and the behavior
+        depends on the concrete implementation of the emitter.
+        
+        """
         self._subscriptions.add(thing)
     
     def discard(self, thing):
+        """ Remove thing from the set of subscribers if present
+        
+        """
         self._subscriptions.discard(thing)
     
     def remove(self, thing):
+        """ remove thing from the set of subscribers
+        
+        raises KeyError if thing is not present
+        
+        """
         self._subscriptions.remove(thing)
     
     def _getstate(self, state):
-        # this is what the __ makes with atrribute names : _{0}{1}.format(ClassName, MethodName)
+        """ pickle protocol: remove the weakset when pickling """
+        # this is what the __ makes with atrribute names:
+        #    _{0}{1}.format(ClassName, MethodName)
         if '_Emitter__subscriptions' in state:
             del state['_Emitter__subscriptions'] # remove the WeakSet
     
@@ -41,5 +66,5 @@ class Emitter(object):
         state = self.__dict__.copy() # copy the dict since we change it
         return self._getstate(_getstate)
     
-    # no need for since this Class can handle a missing _Emitter__subscriptions
     # def __setstate__():
+    # no need for since this Class can handle a missing _Emitter__subscriptions
